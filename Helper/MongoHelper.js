27007b82ddd818e9fe4154bc20b5158cae2f9434
Thong <doanthong002@gmail.com>
2021-04-02 16:52:15 +0700

    update mongo

--- Helper/MongoHelper.js
+++ Helper/MongoHelper.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 export default class MongoHelper{
         static Initialise(){
               try {
+                     //1
                      mongoose.connect(process.env.DATABASE_URL, {
                             useNewUrlParser: true,
                             useUnifiedTopology: true,
